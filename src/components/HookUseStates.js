570de2b59491df1c5fcc4b13be80e84cd605283a
Jheimys Bassani <jheimysbb@gmail.com>
2023-10-11 10:13:11 +0100

    Estudando o hook useState

---
+++ src/components/HookUseStates.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const HookUseStates = () => {
+    let userName = 'João'
+    const [name, setName] = useState('James')
+
+    const changeNames = () => {
+        userName = 'João José'
+
+        setName('James Bassani')
+    }
+    
+    // 2 - usestate e input
+    const [age, setAge] = useState(18)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(age)
+    }
+  return (
+    <div>
+        <h2>UseState</h2>
+        <p>Variável: {userName}</p>
+        <p>useState: {name}</p>
+
+        <button onClick={() => changeNames()}>Mudar nomes</button>
+
+        {/* useState e input */}
+        <p>Digite sua idade:</p>
+        <form onSubmit={handleSubmit}>
+            <input 
+              type='text' 
+              value={age}
+              onChange={(e) => setAge(e.target.value)} 
+            />
+
+            <input type='submit' value="Enviar" />
+        </form>
+        
+        <p>Você tem {age} anos!</p>
+    </div>
+
+  )
+}
+
+export default HookUseStates
---
+++ src/page/Home.js
@@ -0,0 +1,11 @@
+import HookUseStates from "../components/HookUseStates";
+
+const Home = () => {
+    return (
+        <div>
+            <HookUseStates />
+        </div>
+    )
+};
+  
+export default Home;
